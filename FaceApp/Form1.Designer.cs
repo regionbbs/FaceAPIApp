e0cb9241ce9c3eb9c4f0dbe8d856c9a7d3387ab4
小朱 (Jeff Chu) <regionbbs@ms67.url.com.tw>
2020-05-16 16:03:41 +0800

    Application completed commit.

---
+++ FaceApp/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FaceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileName1 = new System.Windows.Forms.TextBox();
+            this.txtFaceInfo1 = new System.Windows.Forms.TextBox();
+            this.txtFaceInfo2 = new System.Windows.Forms.TextBox();
+            this.txtFileName2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cmdVerifyFaces = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(548, 316);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Face Information:";
+            // 
+            // txtFileName1
+            // 
+            this.txtFileName1.Location = new System.Drawing.Point(12, 351);
+            this.txtFileName1.Name = "txtFileName1";
+            this.txtFileName1.Size = new System.Drawing.Size(416, 22);
+            this.txtFileName1.TabIndex = 4;
+            // 
+            // txtFaceInfo1
+            // 
+            this.txtFaceInfo1.Location = new System.Drawing.Point(12, 419);
+            this.txtFaceInfo1.Multiline = true;
+            this.txtFaceInfo1.Name = "txtFaceInfo1";
+            this.txtFaceInfo1.Size = new System.Drawing.Size(548, 175);
+            this.txtFaceInfo1.TabIndex = 5;
+            // 
+            // txtFaceInfo2
+            // 
+            this.txtFaceInfo2.Location = new System.Drawing.Point(591, 419);
+            this.txtFaceInfo2.Multiline = true;
+            this.txtFaceInfo2.Name = "txtFaceInfo2";
+            this.txtFaceInfo2.Size = new System.Drawing.Size(548, 175);
+            this.txtFaceInfo2.TabIndex = 10;
+            // 
+            // txtFileName2
+            // 
+            this.txtFileName2.Location = new System.Drawing.Point(591, 351);
+            this.txtFileName2.Name = "txtFileName2";
+            this.txtFileName2.Size = new System.Drawing.Size(416, 22);
+            this.txtFileName2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(591, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Face Information:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1023, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 24);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Select Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(591, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(548, 316);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cmdVerifyFaces
+            // 
+            this.cmdVerifyFaces.Location = new System.Drawing.Point(14, 615);
+            this.cmdVerifyFaces.Name = "cmdVerifyFaces";
+            this.cmdVerifyFaces.Size = new System.Drawing.Size(1125, 24);
+            this.cmdVerifyFaces.TabIndex = 11;
+            this.cmdVerifyFaces.Text = "Verify faces";
+            this.cmdVerifyFaces.UseVisualStyleBackColor = true;
+            this.cmdVerifyFaces.Click += new System.EventHandler(this.cmdVerifyFaces_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 660);
+            this.Controls.Add(this.cmdVerifyFaces);
+            this.Controls.Add(this.txtFaceInfo2);
+            this.Controls.Add(this.txtFileName2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtFaceInfo1);
+            this.Controls.Add(this.txtFileName1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileName1;
+        private System.Windows.Forms.TextBox txtFaceInfo1;
+        private System.Windows.Forms.TextBox txtFaceInfo2;
+        private System.Windows.Forms.TextBox txtFileName2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button cmdVerifyFaces;
+    }
+}
+
---
+++ FaceApp/Form1.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FaceApp
+{
+    public partial class Form1 : Form
+    {
+        private IList<Guid> _faceIds1;
+        private IList<Guid> _faceIds2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _faceIds1 = new List<Guid>();
+            _faceIds2 = new List<Guid>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.png";
+
+                if (ofd.ShowDialog() == DialogResult.Cancel)
+                    return;
+
+                var file = ofd.FileName;
+                txtFileName1.Text = file;
+                pictureBox1.Image = Image.FromStream(File.OpenRead(file)); 
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                SendFaceAPI(1, txtFaceInfo1, txtFileName1.Text);
+            }
+        }
+
+        private async void SendFaceAPI(int idx, TextBox textbox, string imageSource)
+        {
+            var fa = new List<FaceAttributeType>() {
+                FaceAttributeType.Glasses, 
+                FaceAttributeType.Smile,
+                FaceAttributeType.Gender, 
+                FaceAttributeType.Emotion, //快樂指數
+                FaceAttributeType.Age,
+                FaceAttributeType.FacialHair, //面部毛髮,
+                FaceAttributeType.HeadPose //頭部姿勢
+            };
+
+            var client = GetFaceClient();
+            var list = await client.Face.DetectWithStreamAsync(File.OpenRead(imageSource), returnFaceAttributes: fa);
+
+            if (list.Count == 0)
+                textbox.Text = "No any face detected.";
+            else
+            {
+                foreach (var item in list)
+                {
+                    textbox.Text += $"FaceID：{item.FaceId } \r\n";
+                    textbox.Text += $"性別：{item.FaceAttributes.Gender}\r\n";
+                    textbox.Text += $"年齡：{item.FaceAttributes.Age}\r\n";
+                    textbox.Text += $"微笑：{item.FaceAttributes.Smile}\r\n";
+                    textbox.Text += $"眼鏡：{item.FaceAttributes.Glasses}\r\n";
+                    textbox.Text += $"鬍子：{item.FaceAttributes.FacialHair.Moustache}\r\n";
+                    textbox.Text += $"快樂指數：{item.FaceAttributes.Emotion.Happiness}";
+
+                    if (idx == 1)
+                        _faceIds1.Add(item.FaceId.Value);
+                    else if (idx == 2)
+                        _faceIds2.Add(item.FaceId.Value);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.png";
+
+                if (ofd.ShowDialog() == DialogResult.Cancel)
+                    return;
+
+                var file = ofd.FileName;
+                txtFileName2.Text = file;
+                pictureBox2.Image = Image.FromStream(File.OpenRead(file));
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                SendFaceAPI(2, txtFaceInfo2, txtFileName2.Text);
+            }
+        }
+
+        private async void cmdVerifyFaces_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            var client = GetFaceClient();
+
+            foreach (var faceId1 in _faceIds1)
+            {
+                foreach (var faceId2 in _faceIds2)
+                {
+                    var result = await client.Face.VerifyFaceToFaceAsync(faceId1, faceId2);
+                    if (result.IsIdentical)
+                        count++;
+                }
+            }
+
+            MessageBox.Show("Face comparison identical count: " + count.ToString());
+        }
+
+        private FaceClient GetFaceClient()
+        {
+            var credential = new ApiKeyServiceClientCredentials("face_api_key");
+            var client = new FaceClient(credential)
+            {
+                Endpoint = "https://[face-api-endpoint].cognitiveservices.azure.com"
+            };
+
+            return client;
+        }
+
+        //private async Task SendFaceAPI()
+        //{
+        //    var client = new HttpClient();
+        //    var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        //    // Request headers
+        //    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "51c988f484ad4a11bdbc51a192e5df56");
+
+        //    // Request parameters
+        //    queryString["returnFaceId"] = "true";
+        //    queryString["returnFaceLandmarks"] = "false";
+        //    queryString["returnFaceAttributes"] = "{string}";
+        //    queryString["recognitionModel"] = "recognition_01";
+        //    queryString["returnRecognitionModel"] = "false";
+        //    queryString["detectionModel"] = "detection_01";
+        //    var uri = "https://faceapi-20200516.cognitiveservices.azure.com/face/v1.0/detect?" + queryString;
+
+        //    HttpResponseMessage response;
+
+        //    // Request body
+        //    var stream = new MemoryStream();
+        //    _selectedImage.Save(stream, ImageFormat.Jpeg);
+        //    stream.Flush();
+        //    stream.Position = 0;
+        //    byte[] byteData = stream.ToArray();
+
+        //    using (var content = new ByteArrayContent(byteData))
+        //    {
+        //        content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+        //        response = await client.PostAsync(uri, content);
+        //    }
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var content = await response.Content.ReadAsStringAsync();
+        //        var array = JArray.Parse(content);
+
+        //        var o = array.First().ToObject<JObject>();
+        //        txtFaceID.Text = o.Property("faceId").Value.ToString();
+        //    }
+        //}
+    }
+}
